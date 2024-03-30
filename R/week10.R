0a4666e49e63d3d608692027fbb2b969566ff62e
nickl103 <nickl103@umn.edu>
2024-03-30 18:41:42 -0500

    data visualization complete

--- R/week10.R
+++ R/week10.R
@@ -9,8 +9,12 @@ gss_tbl <- read_sav("../data/GSS2016.sav") %>% #importing data into R
   tibble() %>% #converting to a tibble
   filter(!is.na(MOSTHRS)) %>% #filtering nas from MOSTHRS
   mutate(across(everything(~ifelse(.==0, NA, .)))) %>%
-  rename('work hours' = MOSTHRS) %>% #renaming MOSTHRS to work hours
+  rename(`work hours` = MOSTHRS) %>% #renaming MOSTHRS to work hours
   select(-HRS1, -HRS2) %>% #removing HRS1 and HRS2 from data
   select_if(~mean(is.na(.))<.75) %>% #removing any variables with less than 75% missingness
   mutate_all(as.numeric) #converting to as numeric
 
+#Visualization
+ggplot(gss_tbl, aes(x=`work hours`)) +
+geom_histogram() +
+labs(x= "Work Hours", y= "Frequency", title= "Frequency of Work Hours Histogram")
